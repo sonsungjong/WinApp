a0edc97795f48626ba220a8936e6f3361470a4a9
sonsungjong <tbxmtbfm@naver.com>
2026-06-15 11:21:14 +0900

    update

---
+++ D2DLL/D2DLL.cpp
@@ -0,0 +1,21 @@
+﻿// D2DLL.cpp : DLL을 위해 내보낸 함수를 정의합니다.
+//
+
+#include "framework.h"
+#include "D2DLL.h"
+
+
+// 내보낸 변수의 예제입니다.
+D2DLL_API int nD2DLL=0;
+
+// 내보낸 함수의 예제입니다.
+D2DLL_API int fnD2DLL(void)
+{
+    return 0;
+}
+
+// 내보낸 클래스의 생성자입니다.
+CD2DLL::CD2DLL()
+{
+    return;
+}
